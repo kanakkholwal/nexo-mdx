a2d5351db938f291ea19b4637b788c03b25d41e1
Kanak <kanakkholwal@gmail.com>
2024-06-19 15:07:02 +0530

    Revert "chore: Update npm dependency to latest stable version"
    This reverts commit f4e61f94148e4e87a21cf520eed42259b15340a9.

--- vite.config.ts
+++ vite.config.ts
@@ -3,34 +3,34 @@ import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts';
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
 export default defineConfig({
   plugins: [
     react(),
-    libInjectCss(),
     dts({
-
-      insertTypesEntry: true,
-      copyDtsFiles: true,
-      tsconfigPath: './tsconfig.json',
-    }),],
+    insertTypesEntry: true,
+    copyDtsFiles: true,
+    tsconfigPath: './tsconfig.json',
+  }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./main"),
     },
   },
   build: {
-    copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
-      formats: ['es']
+      name: 'NexoMdx',
+      fileName: (format) => `nexo-mdx.${format}.js`,
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: ['react', 'react-dom'],
       output: {
-        assetFileNames: 'assets/[name][extname]',
-        entryFileNames: '[name].js',
-      }
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        exports: 'named'
+      },
     },
     minify: 'terser', // Use Terser for minification
   }
