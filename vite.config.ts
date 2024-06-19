f4e61f94148e4e87a21cf520eed42259b15340a9
Kanak <kanakkholwal@gmail.com>
2024-06-19 14:03:09 +0530

    chore: Update npm dependency to latest stable version

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,17 @@ import react from '@vitejs/plugin-react-swc';
 import * as path from 'path';
 import { defineConfig } from "vite";
 import dts from 'vite-plugin-dts';
-
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({
-    insertTypesEntry: true,
-    copyDtsFiles: true,
-    tsconfigPath: './tsconfig.json',
-  }),],
+
+      insertTypesEntry: true,
+      copyDtsFiles: true,
+      tsconfigPath: './tsconfig.json',
+    }),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
@@ -20,20 +22,18 @@ export default defineConfig({
     },
   },
   build: {
+    copyPublicDir: false,
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
       name: 'NexoMdx',
-      fileName: (format) => `nexo-mdx.${format}.js`,
+      formats: ['es']
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-        exports: 'named'
-      },
+        assetFileNames: 'assets/[name][extname]',
+        entryFileNames: '[name].js',
+      }
     },
     minify: 'terser', // Use Terser for minification
   }
