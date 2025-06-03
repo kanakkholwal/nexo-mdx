06f06c4e25ff7704cf616d87d71f68a665099b35
Kanak <kanakkholwal@gmail.com>
2025-06-03 23:57:35 +0530

    Refactor ToolBar and Editor components to use classNames for styling; update version in package.json; add classNames utility

--- lib/components/ToolBar/index.tsx
+++ lib/components/ToolBar/index.tsx
@@ -1,6 +1,9 @@
+import { classNames } from '@/classes';
+import i18n from '@/i18n';
 import { cn } from '@/lib/utils';
 import * as React from 'react';
 import Icon from '../Icon';
+
 interface ToolBarProps {
   left?: React.ReactElement[];
   right?: React.ReactElement[];
@@ -11,19 +14,24 @@ interface ToolBarProps {
 export default function ToolBar(props: ToolBarProps) {
   return (
     <div className={cn(
-      `flex flex-wrap gap-4 p-3 bg-white/30 dark:bg-white/10 rounded-xl border border-border dark:border-border/10 backdrop-blur-lg border-opacity-15`,
-      props.isPinned ? 'sticky top-5 left-0 right-0' : 'relative',
-      "nexo-mdx-toolbar"
+      classNames.toolbar.wrapper,
+      props.isPinned ? classNames.toolbar.wrapper_pinned : classNames.toolbar.wrapper_default,
+      "toolbar"
     )}>
-      <div className={cn("inline-flex gap-2 items-center justify-start flex-grow flex-wrap", "nexo-mdx-toolbar_left")}>
-        {props.isPreview ? (<p className="text-sm text-gray-700 dark:text-gray-200 font-semibold inline-flex items-center gap-1">
-          <Icon type="visibility" className="h-4 w-4 inline-block" />
-          <span className="font-bold">Preview Mode</span>
-          <span className="text-xs text-gray-400 dark:text-gray-500 italic"> (See how will it look after render.)</span>
-        </p>) : props.left}
-        <div className={cn("inline-flex gap-2 items-center justify-end ml-auto", "nexo-mdx-toolbar_right")}>
-          {props.right}
-        </div>
+      <div className={cn(classNames.toolbar.container_left, "toolbar_left")}>
+        {props.isPreview ? (
+          <div className={cn(classNames.toolbar.container_preview_div, "toolbar_preview_div")}>
+            <Icon type="visibility" className={cn(classNames.toolbar.container_preview_icon, "toolbar_preview_icon")} />
+            <h5 className={cn(classNames.toolbar.container_preview_title,"toolbar_preview_title")}>
+              {i18n.get("toolbar_preview_title")}
+            </h5>
+            <p className={cn(classNames.toolbar.container_preview_description,"toolbar_preview_description")}>
+              {i18n.get("toolbar_preview_description")}
+            </p>
+          </div>) : props.left}
+      </div>
+      <div className={cn("inline-flex gap-2 items-center justify-end ml-auto", "toolbar_right")}>
+        {props.right}
       </div>
     </div>
   );
